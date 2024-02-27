4a2b291ae5dd578ba4d36845b3733bff3d233870
JoseDavidN <jose.nunez708@pascualbravo.edu.co>
2024-02-27 16:01:38 +0000

    file week 3 correction, Naive_Bayes, entrega_1

--- plot_decision.py
+++ plot_decision.py
@@ -1,6 +1,7 @@
-def plotDecision(offset, res, model, muestras, labels, feature_names):
-  h_min, h_max = muestras[:, 0].min()-offset, muestras[:, 0].max()+offset;
-  v_min, v_max = muestras[:, 1].min()-offset, muestras[:, 1].max()+offset;
+def plotDecision(np, plt, model, samples, labels, set_xlabel, set_ylabel):
+  offset, res = 0.1, 0.01;
+  h_min, h_max = samples[:, 0].min()-offset, samples[:, 0].max()+offset;
+  v_min, v_max = samples[:, 1].min()-offset, samples[:, 1].max()+offset;
 
   h_grid, v_grid = np.meshgrid(np.arange(h_min, h_max, res), np.arange(v_min, v_max, res));
 
@@ -15,6 +16,6 @@ def plotDecision(offset, res, model, muestras, labels, feature_names):
 
   _, ax = plt.subplots(figsize=(8,5))
   ax.pcolormesh(h_grid, v_grid, pred_grid, cmap="Paired")
-  ax.scatter(muestras[:, 0], muestras[:, 1], c=labels, edgecolor="k", cmap="Paired")
-  ax.set_xlabel(feature_names[0])
-  ax.set_ylabel(feature_names[1])
+  ax.scatter(samples[:, 0], samples[:, 1], c=labels, edgecolor="k", cmap="Paired")
+  ax.set_xlabel(set_xlabel)
+  ax.set_ylabel(set_ylabel)
